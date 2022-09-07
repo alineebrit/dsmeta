1510570eb69c17ab7bafdc1abca859cad9e292be
alineebrit <aline.neves@ccc.ufcg.edu.br>
2022-09-07 15:14:03 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,13 @@
-function App() {
-return(
-<h1>Olá mundo!</h1>)
-}
+    import NotificationButton from "./components/NotificationButton"
 
-export default App
+    function App() {
+        return(
+            <>
+                <h1>Olá mundo!</h1>
+                <NotificationButton />
+                <NotificationButton />
+            </>
+        )
+    }
+
+    export default App
